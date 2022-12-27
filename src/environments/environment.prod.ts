5d9e6d03831e2717b69402003fd3c44ed1a840c8
muhammadrafnasp <rafnasbasheer@gmail.com>
2022-12-28 02:00:44 +0530

    TodoApp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyA5jTs3-8ScAeQH5Lu9nVz1WYNBM62liUc",
+    authDomain: "todo-app-d36d3.firebaseapp.com",
+    projectId: "todo-app-d36d3",
+    storageBucket: "todo-app-d36d3.appspot.com",
+    messagingSenderId: "952348572552",
+    appId: "1:952348572552:web:b2c0d07bc17e9ec25a983d"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyA5jTs3-8ScAeQH5Lu9nVz1WYNBM62liUc",
+    authDomain: "todo-app-d36d3.firebaseapp.com",
+    projectId: "todo-app-d36d3",
+    storageBucket: "todo-app-d36d3.appspot.com",
+    messagingSenderId: "952348572552",
+    appId: "1:952348572552:web:b2c0d07bc17e9ec25a983d"
+  }
 };
 
 /*
